474da0dc2d6980e5be77873365eda80925200a86
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-07-17 20:10:36 +0300

    feat: security improved

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+
 import env from "./config/env";
 import errorHandler from "./utils/errorHandler";
 import connectDB from "./config/db";
@@ -10,18 +13,28 @@ import taskRoutes from "./routes/task.route";
 
 const app = express();
 
+// Server Config
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
+// cors config (allows all origins)
+app.use(cors({ origin: true, credentials: true }));
+
+// Health check endpoint
 app.use("/health", (req, res) => res.send("Server is healthy"));
+
+// API routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/tasks", taskRoutes);
 
+// Custom error-handling middleware
 app.use(errorHandler);
 
+// Connect to DB and start server
 const PORT = env.PORT;
-app.listen(PORT, () => {
-	connectDB();
+app.listen(PORT, async () => {
+	await connectDB();
 	console.log(`Server is running on port ${PORT}`);
 });
